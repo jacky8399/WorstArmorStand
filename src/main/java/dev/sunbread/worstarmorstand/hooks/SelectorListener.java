6a386e7c0b21d889766d806e0590a7562fb6ec55
Sunbread <sunbread233@gmail.com>
2022-06-14 14:00:36 +0800

    feat: add in-use notice

--- src/main/java/dev/sunbread/worstarmorstand/hooks/SelectorListener.java
+++ src/main/java/dev/sunbread/worstarmorstand/hooks/SelectorListener.java
@@ -2,7 +2,7 @@ package dev.sunbread.worstarmorstand.hooks;
 
 import dev.sunbread.worstarmorstand.Util;
 import dev.sunbread.worstarmorstand.gui.GUI;
-import org.bukkit.GameMode;
+import org.bukkit.*;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,7 +15,13 @@ public final class SelectorListener implements Listener {
     public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
         if (event.getPlayer().getGameMode() == GameMode.SPECTATOR) return;
         if (!(event.getRightClicked() instanceof ArmorStand as)) return;
-        if (Util.isUsing(event.getPlayer()) || Util.isInUse(as)) return;
+        if (Util.isUsing(event.getPlayer())) return;
+        if (Util.isInUse(as)) {
+            event.getPlayer().playSound(event.getPlayer(), Sound.BLOCK_NOTE_BLOCK_BIT, SoundCategory.MASTER, 1.0F, 0.5F);
+            event.getPlayer().sendTitle(ChatColor.YELLOW + "In Use",
+                    ChatColor.YELLOW + "You can't edit this armor stand at now!", 0, 70, 0);
+            return;
+        }
         if (event.getPlayer().isSneaking()) {
             GUI.createGUI(event.getPlayer(), as);
             event.setCancelled(true);
